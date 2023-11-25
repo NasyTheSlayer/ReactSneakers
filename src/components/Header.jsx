3929e3ad6f021eba22a9f40972db7d026489cc0a
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2023-11-25 21:40:33 +0200

    fix v1.7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Header(props) {
   return (
     <header className="d-flex justify-between align-center p-40">
-      <Link to="/">
+      <Link to="/react-sneakers/">
         <div className="d-flex align-center">
           <img width={40} height={40} src="img/logo.png" alt="headerLogo" />
           <div>
